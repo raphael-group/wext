8d652f34e0fc1b4da9eb803ccc79a47dfb8a5c73
Max Leiserson <mdml@cs.brown.edu>
2016-05-03 09:44:52 -0400

    comment police

--- weighted_exclusivity_test/__init__.py
+++ weighted_exclusivity_test/__init__.py
@@ -8,6 +8,5 @@ from enumerate_sets import *
 from exact import exact_test, py_exact_test
 import cpoibin
 from saddlepoint import saddlepoint
-# import comet_driver as comet
 from comet_exact_test import comet_exact_test
 from exclusivity_tests import weighted_test, unweighted_test
